7510d9bddbf4ff980fd63ceae4e79fc28f98c5ba
ranggarap <fadhilahrangga26@gmail.com>
2026-06-22 11:24:28 +0700

    membuat cetak data mahasiswa

---
+++ CRUDMahasiswaADO/CetakData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDMahasiswaADO
+{
+    internal class CetakData
+    {
+        static string connectionString = "Data Source=LAPTOP-M7PL4IES\\RANGGAFADHILAH;Initial Catalog=DBAkademikADO;User ID=sa;Password=123";
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DAL dbLogic = new DAL();
+
+        ListMahasiswa listMahasiswa = new ListMahasiswa();
+
+        string prodi { get; set; }
+
+        DateTime tglmasuk { get; set; }
+    
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public string Alamat { get; set; }
+        public string NamaProdi { get; set; }
+        public DateTime TanggalDaftar { get; set; }
+    }
+}
+
