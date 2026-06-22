1b8bcfc7f46f267a99e92703a9c2c499ded726d4
ranggarap <fadhilahrangga26@gmail.com>
2026-06-22 11:25:59 +0700

    seting foem 2

---
+++ CRUDMahasiswaADO/Form2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form2 : Form
+    {
+        static string connectionString = "Data Source=LAPTOP-M7PL4IES\\RANGGAFADHILAH;Initial Catalog=DBAkademikADO;User ID=sa ;Password=123";
+
+        // PERBAIKAN: Huruf 'Q' diubah jadi kecil sesuai library bawaan (.NET)
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+        DAL dbLogic = new DAL();
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        // PERBAIKAN: Ditambahkan parameter (object sender, EventArgs e) agar dikenali sebagai Event Form Load
+        private void Form_Load(object sender, EventArgs e)
+        {
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            cmbProdi.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            btnCetak.Enabled = true;
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                SqlCommand cmd = new SqlCommand("select namaprodi from programstudi", conn);
+                cmd.CommandType = CommandType.Text;
+                dtProdi = new DataTable();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(dtProdi);
+                cmbProdi.DataSource = dtProdi;
+                cmbProdi.DisplayMember = "namaprodi";
+                cmbProdi.ValueMember = "namaprodi";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+        }
+
+        // PERBAIKAN: Ditambahkan parameter (object sender, EventArgs e) agar tombol bisa merespon klik
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@inProdi", SqlDbType.VarChar, 50).Value = cmbProdi.SelectedValue;
+                cmd.Parameters.Add("@inTglMsuk", SqlDbType.VarChar, 4).Value = dtpTanggalMasuk.Value.Year.ToString();
+
+                da = new SqlDataAdapter(cmd);
+
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                dataGridView1.DataSource = dtMahasiswa;
+                if (dtMahasiswa.Rows.Count > 0)
+                {
+                    btnCetak.Enabled = true;
+                }
+                else
+                {
+                    btnCetak.Enabled = true;
+                    MessageBox.Show("Data tidak ditemukan");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+        }
+        private void btnCetak_Click(object sender, EventArgs e)
+        {
+            Form3 frm2 = new Form3(cmbProdi.SelectedValue.ToString(), dtpTanggalMasuk.Value);
+            frm2.Show();
+            this.Hide();
+        }
+    }
+}
